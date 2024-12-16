ffab688f4a896b01e58e62448fb105705b1e2b50
zaheer <zaheersamar143@gmail.com>
2024-12-16 12:41:23 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import MyEditor from "./Editor";
+
+function App() {
+
+  return (
+    <div style={{ padding: "20px", fontFamily: "Arial" }}>
+      <MyEditor />
+    </div>
+  );
+}
+
+export default App;
